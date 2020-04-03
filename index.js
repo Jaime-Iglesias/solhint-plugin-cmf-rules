df40918f8ca28b920d3693c3e147a7dd256e4ad0
Jaime <jaime.i.botas@gmail.com>
2020-04-03 18:54:16 +0100

    1.0.1 release no tests

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-class NoFoosAllowed {
+class NoAliases {
     constructor(reporter, config) {
-      this.ruleId = 'no-foos'
+      this.ruleId = 'no-alias'
   
       this.reporter = reporter
       this.config = config
     }
   
-    ContractDefinition(ctx) {
-      const { name } = ctx
-  
-      if (name === 'Foo') {
-        this.reporter.error(ctx, this.ruleId, 'Contracts cannot be named "Foo"')
-      }
+    VariableDeclaration(ctx) {
+        const { typeName } = ctx
+
+        if (typeName.name === 'uint') {
+            this.reporter.error(ctx, this.ruleId, 'avoid using aliases')
+        }
     }
 }
 
@@ -24,7 +24,7 @@ class NoUnderscoreParams {
     }
 
     FunctionDefinition(ctx) {
-        const { parameters } = ctx;
+        const { parameters } = ctx
 
         if (parameters) {
             parameters.forEach((parameter) => {
@@ -36,3 +36,112 @@ class NoUnderscoreParams {
         }
     }
 }
+
+class UndescoreOnPrivateInternalVar {
+    constructor(reporter, config) {
+        this.ruleId = 'underscore-on-private-internal-var'
+    
+        this.reporter = reporter
+        this.config = config
+    }
+
+    StateVariableDeclaration(ctx) {
+        const { variables } = ctx
+
+        if (variables[0].visibility === 'internal' || variables[0].visibility === 'private') {
+            if (!variables[0].name.startsWith('_')) {
+                this.reporter.error(ctx, this.ruleId, 'internal and private variable names must start with _')
+            }
+        } else {
+            if (variables[0].name.startsWith('_')) {
+                this.reporter.error(ctx, this.ruleId, 'only internal and private variable names can start with _')
+            }            
+        }
+    }
+}
+
+class UndescoreOnPrivateInternalFunc {
+    constructor(reporter, config) {
+        this.ruleId = 'underscore-on-private-internal-func'
+    
+        this.reporter = reporter
+        this.config = config
+    }
+
+    FunctionDefinition(ctx) {
+        const { visibility, name} = ctx;
+
+        if (visibility === 'internal' || visibility === 'private') {
+            if (!name.startsWith('_')) {
+                this.reporter.error(ctx, this.ruleId, 'internal and private function names must start with _')
+            }
+        } else {
+            if (name.startsWith('_')) {
+                this.reporter.error(ctx, this.ruleId, 'only internal and private function  names can start with _')
+            }
+        }
+    }
+}
+
+class StateVariableMustBePrivate {
+    constructor(reporter, config) {
+        this.ruleId = 'private-state-variable'
+    
+        this.reporter = reporter
+        this.config = config
+    }
+
+    StateVariableDeclaration(ctx) {
+        const { variables } = ctx
+
+        if (variables[0].visibility != 'private') {
+            this.reporter.error(ctx, this.ruleId, 'all state variables must be private')
+        }
+    }
+}
+
+class UnammedReturns {
+    constructor(reporter, config) {
+        this.ruleId = 'unammed-returns'
+    
+        this.reporter = reporter
+        this.config = config
+    } 
+
+    FunctionDefinition(ctx) {
+        const { returnParameters } = ctx
+
+        if (returnParameters) {
+            returnParameters.forEach((parameter) => {
+                if (parameter.name != null) {
+                    this.reporter.error(ctx, this.ruleId, 'return parameters must be unammed')
+                }
+            })
+        }
+    }
+}
+
+class InterfaceNames {
+    constructor(reporter, config) {
+        this.ruleId = 'interface-names'
+    
+        this.reporter = reporter
+        this.config = config
+    }
+
+    ContractDefinition(ctx) {
+        const { kind, name} = ctx
+
+        if (kind === 'interface') {
+            if (!name.startsWith('I')) {
+                this.reporter.error(ctx, this.ruleId, 'interface names must start with I')
+            }
+        } else {
+            if (name.startsWith('I') || name.startsWith('i')) {
+                this.reporter.error(ctx, this.ruleId, 'only interface names can start with I')
+            }
+        }
+    }
+}
+
+module.exports = [NoAliases, NoUnderscoreParams, UndescoreOnPrivateInternalVar, UndescoreOnPrivateInternalFunc, StateVariableMustBePrivate, UnammedReturns, InterfaceNames]
